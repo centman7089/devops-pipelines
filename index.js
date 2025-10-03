9add5b97d6dab8b9812260b47d9ae67e0db5853d
Centman7089 <innocentimitini@gmail.com>
2025-10-03 13:42:38 +0100

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('test');
+console.log( 'Starting Test' );
+setTimeout( () => console.log( "Waiting for 3 seconds... " ), 3000 );
+setTimeout( () => console.log( "Waiting for tetst... " ), 2000 );
+console.log("tetst Complete");
+
