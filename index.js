767e359084b443f2ad017ade8e737b8c2c06a177
Centman7089 <innocentimitini@gmail.com>
2025-10-03 13:48:06 +0100

    feat: add CI pipeline

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-console.log('test');
-console.log( 'Starting Test' );
-setTimeout( () => console.log( "Waiting for 3 seconds... " ), 3000 );
-setTimeout( () => console.log( "Waiting for tetst... " ), 2000 );
-console.log("tetst Complete");
+console.log( "Hello Dev Ops" );
+console.log("I am Learning CI/CD Pipekline using Github Action");
 
